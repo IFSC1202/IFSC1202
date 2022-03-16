6b83ddd8aff8088b228245ef4456dc8fc00c2fa3
sksharma53 <ssharma@ualr.edu>
2022-03-16 18:04:06 +0000

    m

---
+++ 09.02 Swap Columns.py
@@ -0,0 +1,25 @@
+def swap_columns(a,i,j):
+    for k in range(len(a)):
+        a[k][i],a[k][j] = a[k][j],a[k][i]
+def print_array(a):
+    for i in range(len(a)):
+        for j in range(len(a[i])):
+            print(a[i][j], end = '')
+        print() 
+a = [] 
+numbersfile = open("numbers.txt") 
+x = numbersfile.readline() 
+while x != "":
+    y = x.split(" ")
+    for i in range(len(y)):
+        y[i] = int(y[i])
+    a.append(y)  
+    x = numbersfile.readline()  
+print_array(a)    
+rowcolumnsline = input("Enter the columns to swap:").split()
+i = int(rowcolumnsline[0])
+j = int(rowcolumnsline[1])
+swap_columns(a, i, j)
+print_array(a)
+
+
